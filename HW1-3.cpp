7bdab9578cddf1b4340baa540036b431ca5689fc
yaowalukth <16917yaowaluk@gmail.com>
2024-08-02 01:04:53 +0700

    Update HW1-3.cpp

--- HW1-3.cpp
+++ HW1-3.cpp
@@ -1,4 +1,14 @@
+#include <stdio.h>
 
+int main() {
+
+    char FirstN[10] , Lastn[10] ;
+    printf( "Input Name = " ) ;
+    scanf( "%s %s" , &FirstN , &Lastn ) ;
+    printf( "------------------------------ \n" ) ;
+    printf( "Your name is %s %s" , FirstN , Lastn ) ;
+    return 0 ;
+}
 /*
     เขียนโปรแกรมรับ "ชื่อ" จากผู้ใช้ และแสดงผลบนหน้าจอดังผลลัพธ์ด้านล่างต่อไปนี้ 
     
