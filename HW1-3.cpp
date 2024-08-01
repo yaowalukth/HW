9025eafce4c1d60fd557430560dfaf7dfe04170a
yaowalukth <16917yaowaluk@gmail.com>
2024-08-02 01:04:12 +0700

    Create HW1-3.cpp

---
+++ HW1-3.cpp
@@ -0,0 +1,16 @@
+
+/*
+    เขียนโปรแกรมรับ "ชื่อ" จากผู้ใช้ และแสดงผลบนหน้าจอดังผลลัพธ์ด้านล่างต่อไปนี้ 
+    
+    Test case:
+        First Name: Aomsin
+        Last Name: AomMoney
+    Output:
+        Aomsin AomMoneys TC, RMUTL, Chiang Mai, Thailand
+
+    Test case:
+        First Name: ABC
+        Last Name: DEF
+    Output:
+        ABD DEFs TC, RMUTL, Chiang Mai, Thailand
+*/
