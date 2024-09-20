f0a28157cb02980ffd2f3d3a34de7ad20f343ff1
yaowalukth <16917yaowaluk@gmail.com>
2024-09-20 15:15:42 +0700

    Create HW1-2.cpp

---
+++ HW1/HW1-2.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf( "Hello, I'm Javis (RMUTL-Version). \n" ) ;
+    printf( "This is my first time to coding with C language. \n" ) ;
+    printf( "Today is my birthday. \n" ) ;
+
+    return 0 ;
+}
+//TODO: #6 Fix all bugs.
