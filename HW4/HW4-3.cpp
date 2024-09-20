27258357452d1ec3dde9270897f9b1fd7ca7c7a3
yaowalukth <16917yaowaluk@gmail.com>
2024-09-20 15:31:36 +0700

    Create HW4-3.cpp

---
+++ HW4/HW4-3.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+
+    int a = 0 , b = 0 , c = 0 ;
+    printf( "Enter your number (a b c) : " ) ;
+    scanf( "%d %d %d", &a , &b , &c ) ;
+    printf( "a + b + c = %d", a + b + c ) ;
+    return 0 ;
+}
