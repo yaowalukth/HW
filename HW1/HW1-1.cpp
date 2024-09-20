4d836adb8edf8572f3d8bd8b63637458f6148922
yaowalukth <16917yaowaluk@gmail.com>
2024-09-20 15:15:12 +0700

    Create HW1-1.cpp

---
+++ HW1/HW1-1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf( "Hello World \n" ) ;
+    printf( "My name is Yaowaluk Thajai \n" ) ;
+    printf( "Age = 20 \n" ) ;
+    printf( "Gender = Male \n" ) ;
+    printf( "Address = Chaingmai \n" ) ;
+    printf( "Tel = 062-4057086 \n" ) ;
+
+    return 0 ;
+}
